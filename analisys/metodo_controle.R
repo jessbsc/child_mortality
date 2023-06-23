2e94aa90c520ee4f27de2fa9c289df4268a1918c
jessbsc <jessica.souzab@hotmail.com>
2023-06-23 18:01:26 -0300

    update metodo controle

--- analisys/metodo_controle.R
+++ analisys/metodo_controle.R
@@ -22,40 +22,59 @@ micro_data_csv_clean <- merge(x=micro_data_csv_clean, y=uf_list,
 micro_data_csv_clean$region <- ifelse(micro_data_csv_clean$uf %in%  list('RJ',"SP","MG","ES" ), "Sudeste", ifelse(micro_data_csv_clean$uf %in%  list('RS',"SC","PR"),"Sul",ifelse(micro_data_csv_clean$uf %in%  list('MS',"MT","GO", "DF"),"CentroOeste", ifelse(micro_data_csv_clean$uf %in%  list('AM',"AC","RR","RO","AP","TO", "PA" ),"Norte","Nordeste")))) 
 
 #write.csv(micro_data_csv_clean, "mortality_and_nasc_by_month_v4.csv", row.names=TRUE)
-unique(micro_data_csv_clean$LOCAL_NAME)
+#unique(micro_data_csv_clean$LOCAL_NAME)
 
-mortality_grupo <- filter(micro_data_csv_clean, year_month_date >= '2011/01/01')
-mortality_grupo$month <- format(as.Date(mortality_grupo$year_month_date, format="%d/%m/%Y"),"%m")
-mortality_grupo$year <- format(as.Date(mortality_grupo$year_month_date, format="%d/%m/%Y"),"%Y")
 
-mortality_grupo <- filter(mortality_grupo, LOCAL_NAME == 'São Paulo')
-mortality_grupo$death_per_1000<- mortality_grupo$all_death/1000 
+ano_analise <- '2019'
+ano_final_media <- '2019'
 
-nasc_and_death_by_month_without_city <- mortality_grupo %>% group_by(month) %>% summarise(
-  month = max(month),
-  avg_month = mean(death_per_1000),
-  sd_month = sd(death_per_1000)
-)
+cidade_estudo <- 'São Paulo'
 
-nasc_and_death_by_month_without_city$lim_superior <- nasc_and_death_by_month_without_city$avg_month + 1.96*nasc_and_death_by_month_without_city$sd_month
-nasc_and_death_by_month_without_city$lim_inferior <- nasc_and_death_by_month_without_city$avg_month - 1.96*nasc_and_death_by_month_without_city$sd_month
+diagrama_controle(ano_analise, ano_final_media, cidade_estudo, micro_data_csv_clean)
 
-mortality_grupo_year <- filter(mortality_grupo, year == '2021')
 
-mortality_grupo_year_merge <- merge(x=mortality_grupo_year, y=nasc_and_death_by_month_without_city, 
-                         by.x=c("month"), 
-                         by.y=c("month"))
 
-library(ggplot2)
 
-p <- (ggplot(mortality_grupo_year_merge, aes(x=month, group = 1) ) + 
-        geom_line(aes(y = avg_month), color = "darkred") 
-         + geom_line(aes(y = lim_superior), color = "darkred" , linetype = "dashed") 
-         + geom_line(aes(y = lim_inferior), color = "darkred", linetype = "dashed") 
-         + geom_line(aes(y = death_per_1000), color = "black") 
-         +  ggtitle( paste("Método de Controle"))
-         +    labs(x = "Meses",
-        y = "Incidência de mortalidade por 1000 habitantes")+ theme_bw()
-)
-      
-print(p)
+diagrama_controle = function (ano_analise, ano_final_media, cidade_estudo,  data ) {
+  
+  ano_inicial_media <-  as.numeric(ano_final_media) - 10
+  data$month <- format(as.Date(data$year_month_date, format="%d/%m/%Y"),"%m")
+  data$year <- format(as.Date(data$year_month_date, format="%d/%m/%Y"),"%Y")
+  data$death_per_1000<- data$all_death/1000 
+  
+  mortality_grupo <- filter(data, year >= ano_inicial_media )
+  mortality_grupo <- filter(mortality_grupo, year <= ano_final_media )
+  
+  mortality_grupo <- filter(mortality_grupo, LOCAL_NAME == cidade_estudo)
+  
+  nasc_and_death_by_month_without_city <- mortality_grupo %>% group_by(month) %>% summarise(
+    month = max(month),
+    avg_month = mean(death_per_1000),
+    sd_month = sd(death_per_1000)
+  )
+  
+  nasc_and_death_by_month_without_city$lim_superior <- nasc_and_death_by_month_without_city$avg_month + 1.96*nasc_and_death_by_month_without_city$sd_month
+  nasc_and_death_by_month_without_city$lim_inferior <- nasc_and_death_by_month_without_city$avg_month - 1.96*nasc_and_death_by_month_without_city$sd_month
+  
+  mortality_grupo_year <- filter(data, year == ano_analise)
+  mortality_grupo_year <- filter(mortality_grupo_year, LOCAL_NAME == cidade_estudo)
+  
+  mortality_grupo_year_merge <- merge(x=mortality_grupo_year, y=nasc_and_death_by_month_without_city, 
+                                      by.x=c("month"), 
+                                      by.y=c("month"))
+  
+  library(ggplot2)
+  
+  p <- (ggplot(mortality_grupo_year_merge, aes(x=month, group = 1) ) + 
+          geom_line(aes(y = avg_month), color = "darkred") 
+        + geom_line(aes(y = lim_superior), color = "darkred" , linetype = "dashed") 
+        + geom_line(aes(y = lim_inferior), color = "darkred", linetype = "dashed") 
+        + geom_line(aes(y = death_per_1000), color = "black") 
+        +  ggtitle( paste("Diagrama de Controle - Ano de estudo:", ano_analise, " Range:", ano_final_media,"-", ano_inicial_media, " Cidade:", cidade_estudo ))
+        +    labs(x = "Meses",
+                  y = "Incidência de mortalidade por 1000 habitantes")+ theme_bw()
+  )
+  print(p)
+
+}
+
